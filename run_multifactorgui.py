b8fd2ca1c1546c0b289ff202c348e964913696f4
Abhi Garg <91635257+AbhianshGarg@users.noreply.github.com>
2022-01-28 13:26:49 -0800

    Update run_multifactorgui.py

--- run_multifactorgui.py
+++ run_multifactorgui.py
@@ -1,11 +1,11 @@
 # a215_multi_factor.py
 import tkinter as tk
 import multifactorgui as mfg
-
+  
 # create a multi-factor interface to a restircted app
 my_auth = mfg.MultiFactorAuth()
 
-my_auth.set_authorization("administrator3","1StrongPassword4CSP")
+my_auth.set_authorization("ASDF","1StrongPassword4CSP")
 # confirm authorization info
 auth_info = my_auth.get_authorization()
 print(auth_info)
